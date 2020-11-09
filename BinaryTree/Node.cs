8c47eb9f5b0731b8ce715ec4a038da0624c972b0
Kevin Harvey Jr <lordty@gmail.com>
2020-11-09 16:46:02 -0600

    Added_BinaryNodeTree

---
+++ BinaryTree/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        public Boolean ValueIsSet;
+        private IComparable _value;
+        public Node lower { get; set; }
+        public Node higher { get; set; }
+        public IComparable Value
+        {
+            get => _value; set
+            {
+                ValueIsSet = true;
+                lower = new Node();
+                higher = new Node();
+                _value = value;
+            }
+        }
+
+        public Node()
+        {
+            this.ValueIsSet = false;
+        }
+
+        public Node(IComparable startingValue)
+        {
+            Value = startingValue;
+        }
+
+    }
+}
